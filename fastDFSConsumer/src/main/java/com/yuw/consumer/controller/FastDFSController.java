fc2d0ae283db5aab86e1efee6db8a094afec2e64
XiFYuW <xif_yuw@163.com>
2019-10-02 11:03:19 +0800

    new

--- fastDFSConsumer/src/main/java/com/yuw/consumer/controller/FastDFSController.java
+++ fastDFSConsumer/src/main/java/com/yuw/consumer/controller/FastDFSController.java
@@ -1,11 +1,8 @@
 package com.yuw.consumer.controller;
 
 import com.yuw.consumer.service.FastDFSService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
